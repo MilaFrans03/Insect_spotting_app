1f310ccc43a05592cdc6a468c224215b16aac5d2
Mila Frans <mila.frans@student.ap.be>
2026-01-05 02:42:56 +0100

    Expo go app fix

--- data/_fetcher.js
+++ data/_fetcher.js
@@ -1,3 +1,14 @@
-const fetcher = (...args) => fetch(...args).then(res => res.json())
-
-export default fetcher;
+const fetcher = async (...args) => {
+    const res = await fetch(...args);
+  
+    if (!res.ok) {
+      const text = await res.text();
+      throw new Error(`HTTP ${res.status}: ${text}`);
+    }
+  
+    return res.json();
+  };
+  
+  export default fetcher;
+  
+  
